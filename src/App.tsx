4ba6d8c98215cabb33b5490dfad8a4ad0014a1b7
Katie Agnew <Kate@MacBook-Air-5.local>
2025-12-31 14:38:05 -0600

    test gha

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const problemPrsmWay = [
 const featureCards = [
   {
     id: 'scheduling',
-    title: 'Effortless Scheduling',
+    title: 'Effortless Scheduling2',
     subtitle: 'Let prsm handle the back-and-forth',
     bullets: [
       'Propose meeting times instantly based on preferences and availability',
