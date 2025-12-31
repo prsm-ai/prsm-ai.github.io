e7d0eeb88bf2afdaf13e79cf15ecef0ebf158cee
Katie Agnew <Kate@MacBook-Air-5.local>
2025-12-31 14:31:54 -0600

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
+    outDir: 'docs',
     target: 'es2019'
-  }
+  },
+  plugins: [react()],
 });
