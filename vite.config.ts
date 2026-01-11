97c953ea440d6e52e3303796cb953c6e27855602
Katie Agnew <Kate@MacBook-Air-5.local>
2026-01-11 13:56:38 -0600

    added prerendered

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { prerenderPlugin } from 'vite-prerender-plugin';
 
 export default defineConfig({
   build: {
     outDir: 'docs',
     target: 'es2019'
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerenderPlugin({
+      routes: ['/'],
+      staticDir: 'docs',
+      renderTarget: '#root',
+    }),
+  ],
 });
